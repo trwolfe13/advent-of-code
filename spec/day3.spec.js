2b2099d32d3a98c602a14248274b0e7d25ce31a0
Tom Wolfe <trwolfe13@gmail.com>
2017-12-03 18:23:53 +0000

    Add blank day 3 and 4.

---
+++ spec/day3.spec.js
@@ -0,0 +1,18 @@
+const day3 = require('../src/day3');
+
+describe('day3', () => {
+  describe('part1', () => {
+    it('Produces the correct output', () => {
+      const input = '';
+      const result = day3.part1(input);
+      expect(result).toBe(0);
+    });
+  });
+  describe('part2', () => {
+    if ('Produces the correct output', () => {
+      const input = '';
+      const result = day3.part2(input);
+      expect(result).toBe(0);
+    });
+  });
+});
---
+++ src/day3.js
@@ -0,0 +1,12 @@
+const _ = require('lodash');
+
+module.exports = {
+  part1: function (sequence) {
+    // TODO: Solve problem.
+    return 0;
+  },
+  part2: function (sequence) {
+    // TODO: Solve problem.
+    return 0;
+  }
+}
