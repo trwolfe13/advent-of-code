9f9384e3eaaee00353b223246ed0953a4e1393e5
Tom Wolfe <trwolfe13@gmail.com>
2017-12-04 06:54:17 +0000

    Start implementing spiral.

--- src/day3.js
+++ src/day3.js
@@ -1,5 +1,14 @@
 const _ = require('lodash');
 
+function spiral(maxNumber) {
+  const layers = Math.ceil((Math.ceil(Math.sqrt(maxNumber)) - 1) / 2 + 1);
+  return [
+    [0, 0]
+    [1, 0],
+    [1, -1]
+  ];
+}
+
 module.exports = {
   part1: function (sequence) {
     // TODO: Solve problem.
