b76e65409316e63b0d19dbf89aec5583656ca2dc
Tom Wolfe <trwolfe13@gmail.com>
2017-12-04 11:37:22 +0000

    Solve day 3 part 1.

--- spec/day3.spec.js
+++ spec/day3.spec.js
@@ -1,11 +1,65 @@
 const day3 = require('../src/day3');
 
 describe('day3', () => {
+  describe('move', () => {
+    it('moves up', () => {
+      const direction = day3.Direction.UP;
+      const result = day3.move([0, 0], direction);
+      expect(result).toEqual([0, 1]);
+    });
+    it('moves right', () => {
+      const direction = day3.Direction.RIGHT;
+      const result = day3.move([0, 0], direction);
+      expect(result).toEqual([1, 0]);
+    });
+    it('moves down', () => {
+      const direction = day3.Direction.DOWN;
+      const result = day3.move([0, 0], direction);
+      expect(result).toEqual([0, -1]);
+    });
+    it('moves left', () => {
+      const direction = day3.Direction.LEFT;
+      const result = day3.move([0, 0], direction);
+      expect(result).toEqual([-1, 0]);
+    });
+  });
+  describe('spiral', () => {
+    it('produces correct output at 1', () => {
+      const result = day3.spiral(1);
+      expect(result).toEqual([undefined, [0, 0]]);
+    });
+    it('produces correct output at 10', () => {
+      const result = day3.spiral(10);
+      expect(result).toEqual([
+        undefined, // 0 has no value.
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [0, 1],
+        [-1, 1],
+        [-1, 0],
+        [-1, -1],
+        [0, -1],
+        [1, -1],
+        [2, -1],
+      ]);
+    });
+  });
   describe('part1', () => {
     it('Produces the correct output', () => {
-      const input = '';
+      const input = 1;
       const result = day3.part1(input);
       expect(result).toBe(0);
+    });
+    it('Produces the correct output', () => {
+      const input = 12;
+      const result = day3.part1(input);
+      expect(result).toBe(3);
+    });
+    it('Produces the correct output', () => {
+      const input = 1024;
+      const result = day3.part1(input);
+      expect(result).toBe(31);
     });
   });
   describe('part2', () => {
--- src/day3.js
+++ src/day3.js
@@ -1,19 +1,63 @@
 const _ = require('lodash');
 
+const Direction = {
+  UP: 0,
+  RIGHT: 1,
+  DOWN: 2,
+  LEFT: 3,
+};
+
+function nextDirection(direction) {
+  switch (direction) {
+    case Direction.UP: return Direction.LEFT;
+    case Direction.RIGHT: return Direction.UP;
+    case Direction.DOWN: return Direction.RIGHT;
+    case Direction.LEFT: return Direction.DOWN;
+  }
+}
+
+function move(position, direction) {
+  if (!position) { return [0, 0]; }
+  switch (direction) {
+    case Direction.UP: return [position[0], position[1] + 1];
+    case Direction.RIGHT: return [position[0] + 1, position[1]];
+    case Direction.DOWN: return [position[0], position[1] - 1];
+    case Direction.LEFT: return [position[0] - 1, position[1]];
+  }
+}
+
 function spiral(maxNumber) {
-  const layers = Math.ceil((Math.ceil(Math.sqrt(maxNumber)) - 1) / 2 + 1);
-  return [
-    [0, 0]
-    [1, 0],
-    [1, -1]
-  ];
+  const spiralArray = [undefined];
+  let previousPosition;
+  let direction = Direction.DOWN;
+  let sidePosition = 0;
+  let sideLength = 1;
+  let side = 0;
+
+  for (i = 1; i <= maxNumber; i++) {
+    const position = move(previousPosition, direction);
+    spiralArray.push(position);
+    previousPosition = position;
+
+    sidePosition++;
+    if (sidePosition >= sideLength) {
+      direction = nextDirection(direction);
+      side++;
+      sidePosition = 0;
+      sideLength = Math.ceil(side / 2);
+    }
+  }
+  return spiralArray;
 }
 
 module.exports = {
+  Direction,
+  nextDirection,
+  move,
   spiral,
-  part1: function (sequence) {
-    // TODO: Solve problem.
-    return 0;
+  part1: function (maxNumber) {
+    const coords = spiral(maxNumber)[maxNumber];
+    return Math.abs(coords[0]) + Math.abs(coords[1]);
   },
   part2: function (sequence) {
     // TODO: Solve problem.
