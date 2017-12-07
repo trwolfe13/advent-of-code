ff573515ff0dc72e54884ba6dccecfe7e1983f41
Tom Wolfe <trwolfe13@gmail.com>
2017-12-07 11:27:12 +0000

    Refactor part 1 solution.

--- src/day7/day7.js
+++ src/day7/day7.js
@@ -6,33 +6,49 @@ function parse(input) {
   let match;
   const regex = /(\w+) \((\d+)\)( -> (\w+(, \w+)*))?/g;
   while (match = regex.exec(input)) {
-    output[match[1]] = { name: match[1], weight: Number(match[2]), children: match[4] ? match[4].split(', ') : undefined };
+    output[match[1]] = { name: match[1], weight: Number(match[2]), children: match[4] ? match[4].split(', ') : [] };
   }
-  return output;
+  convertToTreeInPlace(output);
+  console.log(output);
+  return findRoot(output);
 }
 
-function containsAsChild(input, name) {
+function convertToTreeInPlace(input) {
+  Object.keys(input).map(n => input[n]).forEach(node => {
+    node.children = node.children.map(n => input[n]);
+  });
+}
+
+function containsAsChild(input, node) {
   const nodes = Object.keys(input).map(n => input[n]);
-  for(let n = 0; n < nodes.length; n++) {
-    const children = nodes[n].children;
-    if (children && children.includes(name)) {
+  for (let n = 0; n < nodes.length; n++) {
+    if (nodes[n].children.includes(node)) {
       return true;
     }
   }
   return false;
 }
 
-function findRoots(input) {
-  return Object.keys(input).filter(name => !containsAsChild(input, name));
+function findRoot(input) {
+  return Object.keys(input).map(n => input[n]).filter(node => !containsAsChild(input, node))[0];
+}
+
+// function getUnbalanced(input) {
+//   input.children.forEach
+// }
+
+function getWeight(node) {
+  return node.weight + node.children.reduce((total, child) => total + getWeight(child), 0);
 }
 
 module.exports = {
   parse,
   part1: function (input) {
     const tree = parse(input);
-    return findRoots(tree)[0];
+    return tree.name;
   },
   part2: function (input) {
+    const tree = parse(input);
     return 0;
   }
 }
