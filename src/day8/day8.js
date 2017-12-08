cf3162d20534b1eb6e6ddd699fabbbcc086464a5
Tom Wolfe <trwolfe13@gmail.com>
2017-12-08 16:46:09 +0000

    Remove extra indentation.

--- src/day8/day8.js
+++ src/day8/day8.js
@@ -1,27 +1,27 @@
-    const evaluators = {
-      '<': (l, r) => l < r, '<=': (l, r) => l <= r,
-      '>': (l, r) => l > r, '>=': (l, r) => l >= r,
-      '==': (l, r) => l === r, '!=': (l, r) => l !== r,
-    };
-    
-    function execute(input) {
-      const register = { max: 0, values: {} };
-      const regex = /(\w+) (inc|dec) (-?\d+) if (\w+) ([<>!=]{1,2}) (-?\d+)/g;
-      let m;
-      while (m = regex.exec(input)) {
-        if (!evaluators[m[5]](Number(register.values[m[4]] || '0'), Number(m[6]))) { continue; }
-        register.values[m[1]] = (register.values[m[1]] || 0) + (Number(m[3]) * (m[2] === 'dec' ? -1 : 1));
-        register.max = Math.max(register.values[m[1]], register.max);
-      }
-      return register;
-    }
-    
-    module.exports = {
-      part1: function (input) {
-        const register = execute(input);
-        return Math.max(...Object.keys(register.values).map(r => register.values[r]));
-      },
-      part2: function (input) {
-        return execute(input).max;
-      }
-    }
+const evaluators = {
+  '<': (l, r) => l < r, '<=': (l, r) => l <= r,
+  '>': (l, r) => l > r, '>=': (l, r) => l >= r,
+  '==': (l, r) => l === r, '!=': (l, r) => l !== r,
+};
+
+function execute(input) {
+  const register = { max: 0, values: {} };
+  const regex = /(\w+) (inc|dec) (-?\d+) if (\w+) ([<>!=]{1,2}) (-?\d+)/g;
+  let m;
+  while (m = regex.exec(input)) {
+    if (!evaluators[m[5]](Number(register.values[m[4]] || '0'), Number(m[6]))) { continue; }
+    register.values[m[1]] = (register.values[m[1]] || 0) + (Number(m[3]) * (m[2] === 'dec' ? -1 : 1));
+    register.max = Math.max(register.values[m[1]], register.max);
+  }
+  return register;
+}
+
+module.exports = {
+  part1: function (input) {
+    const register = execute(input);
+    return Math.max(...Object.keys(register.values).map(r => register.values[r]));
+  },
+  part2: function (input) {
+    return execute(input).max;
+  }
+}
