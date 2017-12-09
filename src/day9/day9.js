959fa581ca28dcfae5f7150daeeeca02a3aedc31
Tom Wolfe <trwolfe13@gmail.com>
2017-12-09 11:46:07 +0000

    Solve day 9 part 2.

--- src/day9/day9.js
+++ src/day9/day9.js
@@ -1,20 +1,23 @@
 function countGroups (s) {
-  let t = 0, d = 1, g = false, c;
+  let t = 0, d = 1, g = false, f = 0, c;
   for (let n = 0; n < s.length; n++) {
     switch (!!(c = s.charAt(n))) {
       case c === '!': n++; break;
+      case c === '>': g = false; break;
+      case g: { f++; break; }
       case c === '{' && !g: t += d++; break;
       case c === '}' && !g: d--; break;
       case c === '<': g = true; break;
-      case c === '>': g = false; break;
     }
   }
-  return t;
+  return { t, f };
 }
 
 module.exports = {
-  part1: countGroups,
+  part1: function (input) {
+    return countGroups(input).t;
+  },
   part2: function (input) {
-    return 0;
+    return countGroups(input).f;
   }
 }
--- src/day9/day9.spec.js
+++ src/day9/day9.spec.js
@@ -44,8 +44,40 @@ describe('day8', () => {
     });
   });
   describe('part2', () => {
-    it('Produces the correct output', () => {
-
+    it('Produces the correct output 1', () => {
+      const input = '<>';
+      const total = day9.part2(input);
+      expect(total).toEqual(0);
+    });
+    it('Produces the correct output 2', () => {
+      const input = '<random characters>';
+      const total = day9.part2(input);
+      expect(total).toEqual(17);
+    });
+    it('Produces the correct output 3', () => {
+      const input = '<<<<>';
+      const total = day9.part2(input);
+      expect(total).toEqual(3);
+    });
+    it('Produces the correct output 4', () => {
+      const input = '<{!>}>';
+      const total = day9.part2(input);
+      expect(total).toEqual(2);
+    });
+    it('Produces the correct output 5', () => {
+      const input = '<!!>';
+      const total = day9.part2(input);
+      expect(total).toEqual(0);
+    });
+    it('Produces the correct output 6', () => {
+      const input = '<!!!>>';
+      const total = day9.part2(input);
+      expect(total).toEqual(0);
+    });
+    it('Produces the correct output 7', () => {
+      const input = '<{o"i!a,<{i<a>';
+      const total = day9.part2(input);
+      expect(total).toEqual(10);
     });
   });
 });
