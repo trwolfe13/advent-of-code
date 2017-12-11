9c60bae9497936256d7255f0291d6b147ec45c98
Tom Wolfe <trwolfe13@gmail.com>
2017-12-11 16:34:18 +0000

    Solve day 2 part 2.

--- src/2015/day2/day2.js
+++ src/2015/day2/day2.js
@@ -7,6 +7,9 @@ module.exports = {
     }, 0);
   },
   part2: function (input) {
-    return 0;
+    return input.split('\n').reduce((p, c) => {
+      const [l, w, h] = [...c.split('x').map(Number)];
+      return p + Math.min(2 * (l + w), 2 * (w + h), 2 * (h + l)) + l * w * h;
+    }, 0);
   }
 }
