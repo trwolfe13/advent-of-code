dff7893cb6641705a7112b1813a507bcf4b3f417
Tom Wolfe <trwolfe13@gmail.com>
2017-12-11 15:54:25 +0000

    Move into 2017 folder.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  context: path.join(__dirname, './src/day8/graph'),
+  context: path.join(__dirname, './src/2017/day8/graph'),
   target: 'web',
   entry: [
     './index.js'
