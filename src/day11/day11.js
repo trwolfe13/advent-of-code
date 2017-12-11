b69a2033ea9d45341dff43ffec0f7db4256a8a80
Tom Wolfe <trwolfe13@gmail.com>
2017-12-11 08:35:44 +0000

    Solve day 11 part 1.

---
+++ src/day11/day11.js
@@ -0,0 +1,32 @@
+const move = {
+  'n': ([x, y]) => [x, y - 1],
+  's': ([x, y]) => [x, y + 1],
+  'nw': ([x, y]) => [x - 1, y],
+  'sw': ([x, y]) => [x - 1, y + 1],
+  'ne': ([x, y]) => [x + 1, y - 1],
+  'se': ([x, y]) => [x + 1, y],
+};
+
+function distance(end, start = [0, 0]) {
+  const dx = end[0] - start[0];
+  const dy = end[1] - start[0];
+  if (Math.sign(dx) === Math.sign(dy)) {
+    return Math.abs(dx) + Math.abs(dy);
+  } else {
+    return Math.max(Math.abs(dx), Math.abs(dy));
+  }
+}
+
+module.exports = {
+  move,
+  part1: function (input) {
+    let coord = [0, 0];
+    input.split(',').forEach(step => {
+      coord = move[step](coord);
+    });
+    return distance(coord);
+  },
+  part2: function (input) {
+    return 0;
+  }
+}
---
+++ src/day11/day11.spec.js
@@ -0,0 +1,47 @@
+const day11 = require('./day11');
+
+describe('day8', () => {
+  describe('move', () => {
+    it('moves north', () => {
+      expect(day11.move['n']([0, 0])).toEqual([0, -1]);
+    });
+    it('moves south', () => {
+      expect(day11.move['s']([0, 0])).toEqual([0, 1]);
+    });
+    it('moves north west', () => {
+      expect(day11.move['nw']([0, 0])).toEqual([-1, 0]);
+    });
+    it('moves south west', () => {
+      expect(day11.move['sw']([0, 0])).toEqual([-1, 1]);
+    });
+    it('moves north east', () => {
+      expect(day11.move['ne']([0, 0])).toEqual([1, -1]);
+    });
+    it('moves south east', () => {
+      expect(day11.move['se']([0, 0])).toEqual([1, 0]);
+    });
+  });
+  describe('part1', () => {
+    it('Produces the correct output 1', () => {
+      const input = 'ne,ne,ne';
+      expect(day11.part1(input)).toEqual(3);
+    });
+    it('Produces the correct output 2', () => {
+      const input = 'ne,ne,sw,sw';
+      expect(day11.part1(input)).toEqual(0);
+    });
+    it('Produces the correct output 3', () => {
+      const input = 'ne,ne,s,s';
+      expect(day11.part1(input)).toEqual(2);
+    });
+    it('Produces the correct output 4', () => {
+      const input = 'se,sw,se,sw,sw';
+      expect(day11.part1(input)).toEqual(3);
+    });
+  });
+  describe('part2', () => {
+    it('Produces the correct output 1', () => {
+
+    });
+  });
+});
