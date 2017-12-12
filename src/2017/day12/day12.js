413bc20806766f071f82e544f2e56c3421edcf93
Tom Wolfe <trwolfe13@gmail.com>
2017-12-12 08:43:34 +0000

    Solve day 12 part 1.

--- src/2017/day12/day12.js
+++ src/2017/day12/day12.js
@@ -1,13 +1,14 @@
+function map(g, l, n = 0) {
+  if (l.includes(n)) { return; }
+  l.push(n);
+  g[n].forEach(c => map(g, l, c));
+}
+
 module.exports = {
   part1: function (input) {
     const z = [];
-    const graph = input.match(/[^\r\n]+/g).map(n => n.substring(n.indexOf('>') + 2).trim().split(', ').map(Number));
-    graph.forEach((c, n) => {
-      if (!z.includes(n) && (n === 0 || c.includes(0) || c.filter(x => z.includes(x)).length > 0)) {
-        z.push(n, ...c.filter(x => !c.includes(x)));
-      }
-    });
-    console.log(z);
+    const g = input.match(/[^\r\n]+/g).map(n => n.substring(n.indexOf('>') + 2).trim().split(', ').map(Number));
+    map(g, z);
     return z.length;
   },
   part2: function (input) {
