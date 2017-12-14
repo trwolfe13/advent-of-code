e040c8b67144a589fc2afe08b424535ab7410a51
Tom Wolfe <trwolfe13@gmail.com>
2017-12-14 08:40:11 +0000

    I am a monster.

--- src/2017/day14/day14.js
+++ src/2017/day14/day14.js
@@ -1,17 +1,13 @@
 const day10 = require('../day10/day10');
 
 function toBinary(hex) {
-  const bits = [];
-  for (var n = 0; n < hex.length; n++) {
-    bits.push(...parseInt(hex[n], 16).toString(2).padStart(4, '0').split('').map(Number));
-  }
-  return bits;
+  return hex.split('').map(h => parseInt(h, 16).toString(2).padStart(4, '0').split('').map(Number)).reduce((p, c) => p.concat(c));
 }
 
 module.exports = {
   toBinary,
   part1: function (input) {
-    return[...Array(128).keys()].map(k => day10.part2(input + '-' + k.toString())).map(toBinary).map(b => b.filter(i => i).length).reduce((p, c) => p + c, 0);
+    return [...Array(128).keys()].map(k => day10.part2(`${input}-${k}`)).map(toBinary).map(b => b.filter(i => i).length).reduce((p, c) => p + c, 0);
   },
   part2: function (input) {
     return 0;
