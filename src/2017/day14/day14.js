ea7108802c54b1375e138d4d3f76d5278998dc2f
Tom Wolfe <trwolfe13@gmail.com>
2017-12-14 10:03:43 +0000

    Refactor day 14.

--- src/2017/day14/day14.js
+++ src/2017/day14/day14.js
@@ -1,15 +1,20 @@
 const day10 = require('../day10/day10');
 
-function toBinary(hex) {
-  return hex.split('').map(h => parseInt(h, 16).toString(2).padStart(4, '0').split('').map(Number)).reduce((p, c) => p.concat(c));
-}
+const list = n => [...Array(n).keys()];
+const hash = i => k => day10.part2(`${i}-${k}`)
+const truthCount = a => a.filter(e => e).length;
+const sum = (p, c) => p + c;
+const concat = (p, c) => p.concat(c);
+const hex2bin = hex => hex.split('').map(h => parseInt(h, 16).toString(2).padStart(4, '0').split('').map(Number)).reduce(concat);
+const binary = i => list(128).map(hash(i)).map(hex2bin);
 
 module.exports = {
-  toBinary,
+  toBinary: hex2bin,
   part1: function (input) {
-    return [...Array(128).keys()].map(k => day10.part2(`${input}-${k}`)).map(toBinary).map(b => b.filter(i => i).length).reduce((p, c) => p + c, 0);
+    return binary(input).map(truthCount).reduce(sum, 0);
   },
   part2: function (input) {
-    return 0;
+    const d = binary(input);
+    console.log(d.map(f => f.map(String).join('')).join('\n'));
   }
 }
