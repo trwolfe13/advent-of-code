503d38b736255f8f40ec7d834b53194d60aa33c9
Tom Wolfe <trwolfe13@gmail.com>
2017-12-15 09:03:24 +0000

    Obfuscated day15.

--- src/day15/day15.js
+++ src/day15/day15.js
@@ -1,26 +1,15 @@
-function gen(f, m = 1) {
-  return s => {
-    let n = s;
-    do { n = (n * f) % 2147483647; } while ((n % m) !== 0)
-    return n;
+function g(f, s, m = 1) {
+  let n = s;
+  return () => {
+    do { n = (n * f) % 0x7FFFFFFF; } while ((n % m) !== 0)
+    return n & 0xFFFF;
   }
 }
-
-function count(a, b, c, am = 1, bm = 1) {
-  const ag = gen(16807, am), bg = gen(48271, bm);
-  let t = 0, ap = a, bp = b;
-  for (let n = 0; n < c; n++) {
-    if ((ap & 65535) === (bp & 65535)) { t++; }
-    ap = ag(ap); bp = bg(bp);
-  }
-  return t;
+function t(a, b, c, am = 1, bm = 1) {
+  const ag = g(16807, a, am), bg = g(48271, b, bm);
+  return [...Array(c).keys()].filter(() => ag() === bg()).length;
 }
-
 module.exports = {
-  part1: function (a, b, c) {
-    return count(a, b, c);
-  },
-  part2: function (a, b, c) {
-    return count(a, b, c, 4, 8);
-  }
+  part1: () => t(618, 814, 40000000),
+  part2: () => t(618, 814, 5000000, 4, 8)
 }
--- src/day15/index.js
+++ src/day15/index.js
@@ -2,11 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const day15 = require('./day15');
 
-const input1 = 618;
-const input2 = 814;
 
-let answer = day15.part1(input1, input2, 40000000);
+let answer = day15.part1();
 console.log('Part 1', answer);
 
-answer = day15.part2(input1, input2, 5000000);
+answer = day15.part2();
 console.log('Part 2', answer);
