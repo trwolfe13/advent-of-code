25bdf00845fd27af4022bb5960896adc08003237
Tom Wolfe <trwolfe13@gmail.com>
2017-12-15 09:07:03 +0000

    Fix day15.

--- src/day15/day15.js
+++ src/day15/day15.js
@@ -1,15 +1,15 @@
-function g(f, s, m = 1) {
+function g(f, s, m) {
   let n = s;
   return () => {
     do { n = (n * f) % 0x7FFFFFFF; } while ((n % m) !== 0)
     return n & 0xFFFF;
   }
 }
-function t(a, b, c, am = 1, bm = 1) {
+function t(a, b, c, am, bm) {
   const ag = g(16807, a, am), bg = g(48271, b, bm);
   return [...Array(c).keys()].filter(() => ag() === bg()).length;
 }
 module.exports = {
-  part1: () => t(618, 814, 40000000),
-  part2: () => t(618, 814, 5000000, 4, 8)
+  part1: (a, b, c) => t(a, b, c, 1, 1),
+  part2: (a, b, c) => t(a, b, c, 4, 8)
 }
--- src/day15/index.js
+++ src/day15/index.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const day15 = require('./day15');
 
-
-let answer = day15.part1();
+let answer = day15.part1(618, 814, 40000000);
 console.log('Part 1', answer);
 
-answer = day15.part2();
+answer = day15.part2(618, 814, 5000000);
 console.log('Part 2', answer);
