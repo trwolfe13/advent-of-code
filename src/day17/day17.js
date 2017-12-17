726a91b1723a3c3c7d5d0643e4b303cf55ee11cc
Tom Wolfe <trwolfe13@gmail.com>
2017-12-17 16:25:30 +0000

    Solve part 2.

--- src/day17/day17.js
+++ src/day17/day17.js
@@ -6,7 +6,15 @@ function spin (t) {
   return b[p + 1 % b.length];
 }
 
+function spin2 (t) {
+  let p = 0, o = 1;
+  for (let n = 1; n < 50000000; n++) {
+    if ((p = (p + t) % n + 1) === 1) { o = n; }
+  }
+  return o;
+}
+
 module.exports = {
   part1: i => spin(i),
-  part2: i => 0,
+  part2: i => spin2(i),
 }
