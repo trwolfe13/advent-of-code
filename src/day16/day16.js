b544f2ad37586515eadc6c783f71bb40f7d67b27
Tom Wolfe <trwolfe13@gmail.com>
2017-12-17 12:48:12 +0000

    Solve day 16.

--- src/day16/day16.js
+++ src/day16/day16.js
@@ -7,15 +7,22 @@ const move = {
   'p': (d, i) => swap(d, [d.indexOf(i[0]), d.indexOf(i[2])])
 }
 
-function dance (m) {
+function dance (m, t = 1) {
   let d = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'];
-  m.forEach(i => d = move[i[0]](d, i.slice(1)));
+  const s = d.join('');
+
+  let i = t;
+  for (let n = 1; n <= t; n++) {
+    m.forEach(i => d = move[i[0]](d, i.slice(1)));
+    if (d.join('') === s) { i = t % n; break; }
+    i--;
+  }
+
+  while (i > 0) { m.forEach(i => d = move[i[0]](d, i.slice(1))); i--; }
   return d;
 }
 
 module.exports = {
-  part1: i => dance(parse(i)).join(''),
-  part2: function (input) {
-    return 0;
-  }
+  part1: i => dance(parse(i), 1).join(''),
+  part2: i => dance(parse(i), 1000000000).join(''),
 }
--- src/day16/index.js
+++ src/day16/index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const day15 = require('./day16');
+const day16 = require('./day16');
 
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
 
-let answer = day15.part1(input);
+let answer = day16.part1(input);
 console.log('Part 1', answer);
 
-answer = day15.part2(input);
+answer = day16.part2(input);
 console.log('Part 2', answer);
