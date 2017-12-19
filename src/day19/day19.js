334074db6a7d4a5499b282b95d5d7cee1829dab2
Tom Wolfe <trwolfe13@gmail.com>
2017-12-19 08:52:34 +0000

    Solve day 19.

---
+++ src/day19/day19.js
@@ -0,0 +1,69 @@
+const Direction = { UP: 1, DOWN: 2, LEFT: 3, RIGHT: 4 }
+const VTRAVEL = '|', HTRAVEL = '-', CHANGED = '+';
+
+const parse = i => i.match(/[^\r\n]+/g).map(l => l.split(''));
+const char = (g, p) => g[p[1]][p[0]];
+const positionValid = (g, p) => g[p[1]] && char(g, p);
+const move = (pos, dir) => {
+  switch (dir) {
+    case Direction.UP: return [pos[0], pos[1] - 1];
+    case Direction.DOWN: return [pos[0], pos[1] + 1];
+    case Direction.LEFT: return [pos[0] - 1, pos[1]];
+    case Direction.RIGHT: return [pos[0] + 1, pos[1]];
+    default: throw new Error(`Unrecognised direction: ${dir}`);
+  }
+};
+
+function findStart(graph) {
+  for (let x = 0; x < graph[0].length; x++) {
+    if (graph[0][x] === VTRAVEL) { return [x, 0]; }
+  }
+}
+
+function findDirection(graph, position, direction) {
+  if (direction === Direction.UP || direction === Direction.DOWN) {
+    const lChar = char(graph, move(position, Direction.LEFT));
+    if (lChar === HTRAVEL ||lChar.match(/[A-Z]/)) {
+      return Direction.LEFT;
+    } else {
+      return Direction.RIGHT;
+    }
+  } else {
+    const uChar = char(graph, move(position, Direction.UP))
+    if (uChar === VTRAVEL || uChar.match(/[A-Z]/)) {
+      return Direction.UP;
+    } else {
+      return Direction.DOWN;
+    }
+  }
+}
+
+function walk(graph) {
+  let position = findStart(graph), direction = Direction.DOWN, buffer = '';
+  while (positionValid(graph, position)) {
+    let c = char(graph, position);
+    console.log('C: ', position, buffer);
+    switch (c) {
+      case VTRAVEL: case HTRAVEL: break;
+      case CHANGED: direction = findDirection(graph, position, direction); break;
+      case ' ': return buffer;
+      default: buffer += c; break;
+    }
+    position = move(position, direction);
+  }
+  return buffer.trim();
+}
+
+module.exports = {
+  walk,
+  findStart,
+  parse,
+  part1: function (input) {
+    const graph = parse(input);
+    const path = walk(graph);
+    return path;
+  },
+  part2: function (input) {
+    return 0;
+  }
+}
---
+++ src/day19/day19.spec.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const day19 = require('./day19');
+
+describe('day18', () => {
+
+  const input = fs.readFileSync(path.join(__dirname, './input.test.txt'), 'utf8');
+
+  describe('findStart', () => {
+    it('should find the start point', () => {
+      const graph = day19.parse(input);
+      expect(day19.findStart(graph)).toEqual([5, 0]);
+    });
+  });
+
+  describe('part1', () => {
+    it('Produces the correct output 1', () => {
+      expect(day19.part1(input)).toBe('ABCDEF');
+    });
+  });
+
+  describe('part2', () => {
+    it('Produces the correct output 1', () => {
+
+    });
+  });
+});
