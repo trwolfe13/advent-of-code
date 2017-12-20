477ee46781ac64da8cbaaebc171c211e94314dd6
Tom Wolfe <trwolfe13@gmail.com>
2017-12-20 07:27:11 +0000

    Add move function.

--- src/day20/day20.js
+++ src/day20/day20.js
@@ -5,6 +5,15 @@ const parsePart = (def, part) => {
 const parseParticle = def => ({ p: parsePart(def, 'p'), v: parsePart(def, 'v'), a: parsePart(def, 'a') });
 const parse = i => i.match(/[^\r\n]+/g).map(parseParticle);
 
+const move = p => {
+  p.v.0 += p.a.0;
+  p.v.1 += p.a.1;
+  p.v.2 += p.a.2;
+  p.p.0 += p.v.0;
+  p.p.1 += p.v.1;
+  p.p.2 += p.v.2;
+}
+
 module.exports = {
   part1: function (input) {
     return parse(input);
