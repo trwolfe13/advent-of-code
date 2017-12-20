50c74a4ad316bafc260b912e488594b01066cabc
Tom Wolfe <trwolfe13@gmail.com>
2017-12-20 07:20:19 +0000

    Add day 20 parsing.

--- src/day20/day20.js
+++ src/day20/day20.js
@@ -1,6 +1,13 @@
+const parsePart = (def, part) => {
+  let i = def.indexOf(`${part}=<`) + 3;
+  return def.substring(i, def.indexOf('>', i)).split(',').map(Number);
+};
+const parseParticle = def => ({ p: parsePart(def, 'p'), v: parsePart(def, 'v'), v: parsePart(def, 'a') });
+const parse = i => i.match(/[^\r\n]+/g).map(parseParticle);
+
 module.exports = {
   part1: function (input) {
-    return 0;
+    return parse(input);
   },
   part2: function (input) {
     return 0;
