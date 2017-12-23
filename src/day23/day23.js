986c507a3e7073dd6761f59789dd06f9895e695b
Tom Wolfe <trwolfe13@gmail.com>
2017-12-23 18:04:32 +0000

    Simulate program.

--- src/day23/day23.js
+++ src/day23/day23.js
@@ -11,15 +11,71 @@ const instructions = {
   jnz: (i, r, x, y) => { if (i.get(r, x) !== 0) { r._ += i.get(r, y) } else { r._++ } }
 }
 
+function p2 () {
+  let a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
+
+  b = 65;
+  c = b;
+
+  if (a) {
+    b *= 100;
+    b -= -100000;
+    c = b;
+    c -= -17000;
+  }
+
+  do {
+    f = 1;
+    d = 2;
+
+    do {
+      e = 2;
+
+      do {
+        g = d;
+        g *= e;
+        g -= b;
+
+        if (!g) {
+          f = 0;
+        }
+        e -= -1;
+        g = e;
+        g -= b;
+
+      } while (g);
+
+      d += 1;
+      g = d;
+      g -= b;
+
+    } while (g);
+
+    if (!f) {
+      h -= -1;
+    }
+
+    g = b;
+    g -= c;
+
+    if (!g) {
+      return { a, b, c, d, e, f, g, h };
+    }
+    b -= -17;
+  } while (true)
+}
+
+
 module.exports = {
   part1: function (input) {
     const i = parse(input), r = registers();
     while (!terminated(r, i)) {
       execute(r, i);
     }
+    console.log(r);
     return r.mul;
   },
   part2: function (input) {
-    return 0;
+    console.log(p2());
   }
 }
