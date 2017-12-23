660d9b35bf04754a76c78bbdd5aaed505edbdea8
Tom Wolfe <trwolfe13@gmail.com>
2017-12-23 12:24:18 +0000

    Make progress on day 21.

--- src/day21/day21.js
+++ src/day21/day21.js
@@ -1,5 +1,38 @@
+const _ = require('lodash');
 const hashToInt = c => c === '#' ? 1 : 0;
 const mapHashToInt = r => r.split('').map(hashToInt);
+
+const twoByTwoSquare = (i, x, y) => {
+  const xn = 2 * x, yn = 2 * y;
+  return [
+    i[xn + 0][yn + 0], i[xn + 0][yn + 1],
+    i[xn + 1][yn + 0], i[xn + 1][yn + 1]
+  ];
+};
+
+const threeByThreeSquare = (i, x, y) => {
+  const xn = 3 * x, yn = 3 * y;
+  return [
+    i[xn + 0][yn + 0], i[xn + 0][yn + 1], i[xn + 0][yn + 2],
+    i[xn + 1][yn + 0], i[xn + 1][yn + 1], i[xn + 1][yn + 2],
+    i[xn + 2][yn + 0], i[xn + 2][yn + 1], i[xn + 2][yn + 2]
+  ];
+};
+
+const twoByTwo = i => [twoByTwoSquare(i, 0, 0)];
+const fourByFour = i => [twoByTwoSquare(i, 0, 0), twoByTwoSquare(i, 0, 1), twoByTwoSquare(i, 1, 0), twoByTwoSquare(i, 1, 1)];
+
+const threeByThree = i => [threeByThreeSquare(i, 0, 0)];
+const sixBySix = i => [threeByThreeSquare(i, 0, 0), threeByThreeSquare(i, 0, 1), threeByThreeSquare(i, 1, 0), threeByThreeSquare(i, 1, 1)];
+
+const sixBySixReverse = i => [
+  [i[0][0], i[0][1], i[0][2], i[1][0], i[1][1], i[1][2]],
+  [i[0][3], i[0][4], i[0][5], i[1][3], i[1][4], i[1][5]],
+  [i[0][6], i[0][7], i[0][8], i[1][6], i[1][7], i[1][8]],
+  [i[2][0], i[2][1], i[2][2], i[3][0], i[3][1], i[3][2]],
+  [i[2][3], i[2][4], i[2][5], i[3][3], i[3][4], i[3][5]],
+  [i[2][6], i[2][7], i[2][8], i[3][6], i[3][7], i[3][8]],
+];
 
 const start = [
   [0, 1, 0],
@@ -15,12 +48,46 @@ const parse = i => {
   return Object.assign({}, ...o);
 }
 
+const split = image => {
+  switch (image.length) {
+    case 2: return twoByTwo(image);
+    case 3: return threeByThree(image);
+    case 4: return fourByFour(image);
+    case 6: return sixBySix(image);
+    default: throw new Error(`Unhandled image size: ${image.length}.`);
+  }
+}
+
+const match = (piece, patterns) => {
+  const chunk = _.chunk(piece, Math.sqrt(piece.length));
+  const key = chunk.map(p => p.join('')).join('/');
+
+  // Base case.
+  if (patterns[key]) return patterns[key];
+
+  // TODO: Implement rotations.
+  throw new Error(`Unrecognized pattern: ${key}`);
+}
+
+const join = pieces => {
+  if (pieces.length === 1) { return _.chunk(pieces[0], Math.sqrt(pieces[0].length)); }
+  switch (pieces[0].length) {
+    case 4: return fourByFour(pieces);
+    case 9: return sixBySixReverse(pieces);
+    default: throw new Error(`Unhandled image size ${pieces[0].length}`);
+  }
+}
+
 module.exports = {
   parse,
+  split,
+  match,
+  join,
   part1: function (input) {
-    let image = parse(input);
+    let image = start, patterns = parse(input);
     for (let n = 0; n <= 5; n++) {
-
+      const pieces = split(image).map(p => match(p, patterns));
+      image = join(pieces);
     }
     return 0;
   },
--- src/day21/day21.spec.js
+++ src/day21/day21.spec.js
@@ -13,13 +13,129 @@ describe('day21', () => {
           [0, 0, 1, 0],
           [0, 0, 0, 1],
         ]
-      })
+      });
+    });
+  });
+
+  describe('split', () => {
+    it('Produces the correct output 4x4', () => {
+      const input = [
+        [1, 1, 0, 1],
+        [1, 0, 1, 1],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+      ];
+      expect(day21.split(input)).toEqual([
+        [1, 1, 1, 0],
+        [0, 1, 1, 1],
+        [0, 0, 0, 0],
+        [1, 0, 0, 1]
+      ]);
+    });
+    it('Produces the correct output 6x6', () => {
+      const input = [
+        [1, 1, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ];
+      expect(day21.split(input)).toEqual([
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+      ]);
+    });
+  });
+
+  describe('join', () => {
+    it('Produces the correct output 4x4', () => {
+      const input = [
+        [1, 1, 1, 0],
+        [0, 1, 1, 1],
+        [0, 0, 0, 0],
+        [1, 0, 0, 1]
+      ];
+      const join = day21.join(input);
+      expect(join).toEqual([
+        [1, 1, 0, 1],
+        [1, 0, 1, 1],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+      ]);
+    });
+    it('Produces the correct output 6x6', () => {
+      const input = [
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0, 0],
+      ];
+      const join = day21.join(input);
+      expect(join).toEqual([
+        [1, 1, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+      ]);
+    });
+  });
+
+  describe('match', () => {
+    it('Finds an exact match', () => {
+      const input = [1, 1, 0, 1];
+      const patterns = {
+        '11/01': [0, 0, 1, 0],
+      };
+      const match = day21.match(input, patterns);
+      expect(match).toEqual([0, 0, 1, 0]);
     });
   });
 
   describe('part1', () => {
-    it('Produces the correct output 1', () => {
-
+    it('Produces the correct output 3x3', () => {
+      const input = [
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1]
+      ];
+      const patterns = {
+        '010/001/111': [0, 1, 0, 0, 0, 1, 1, 1, 1]
+      };
+      const split = day21.split(input);
+      const matches = split.map(p => day21.match(p, patterns));
+      const join = day21.join(matches);
+      expect(join).toEqual([
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 1, 1]
+      ]);
+    });
+    it('Produces the correct output 4x4', () => {
+      const input = [
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+      ];
+      const patterns = {
+        '01/10': [0, 1, 0, 0, 0, 1, 1, 1, 1]
+      };
+      const split = day21.split(input);
+      const matches = split.map(p => day21.match(p, patterns));
+      const join = day21.join(matches);
+      expect(join).toEqual([
+        [0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1],
+      ]);
     });
   });
 
