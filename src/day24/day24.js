a8e2ee7b31eda600701d9f8bc4fa80d3b3b1bb0f
Tom Wolfe <trwolfe13@gmail.com>
2017-12-24 11:36:26 +0000

    Solve day 24.

--- src/day24/day24.js
+++ src/day24/day24.js
@@ -6,7 +6,8 @@ function buildTree (node, connectors, parent = undefined, using = 0, weight = 0)
     children: [],
     parent,
     using,
-    weight: node[0] + node[1] + (parent ? parent.weight : 0)
+    weight: node[0] + node[1] + (parent ? parent.weight : 0),
+    length: 1 + (parent ? parent.length : 0),
   };
 
   const availablePort = node.filter(p => p !== using)[0] || node[0];
@@ -28,7 +29,18 @@ const trees = connectors => {
 
 function strongestPath (node) {
   if (node.children.length === 0) { return node.weight; }
-  return Math.max(...node.children.map(c => strongestPath(c)));
+  return Math.max(...node.children.map(strongestPath));
+}
+
+function andrexPath (node) {
+  let andrexNode = node;
+  node.children.forEach(child => {
+    const aNode = andrexPath(child);
+    if (!andrexNode || aNode.length > andrexNode.length || aNode.length === andrexNode.length && aNode.weight > andrexNode.weight) {
+      andrexNode = aNode;
+    }
+  })
+  return andrexNode;
 }
 
 module.exports = {
@@ -40,9 +52,13 @@ module.exports = {
     const roots = connectors.filter(c => c[0] === 0 || c[1] === 0);
     const nonRoots = connectors.filter(c => !roots.includes(c));
     const trees = roots.map(root => buildTree(root, nonRoots));
-    return Math.max(...trees.map(tree => strongestPath(tree)));
+    return Math.max(...trees.map(strongestPath));
   },
-  part2: function () {
-    return 0;
+  part2: function (input) {
+    const connectors = parse(input);
+    const roots = connectors.filter(c => c[0] === 0 || c[1] === 0);
+    const nonRoots = connectors.filter(c => !roots.includes(c));
+    const trees = roots.map(root => buildTree(root, nonRoots));
+    return Math.max(...trees.map(andrexPath).map(n => n.weight));
   }
 }
--- src/day24/index.js
+++ src/day24/index.js
@@ -4,8 +4,8 @@ const day24 = require('./day24')
 
 const input = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
 
-let answer = day24.part1(input);
-console.log('Part 1', answer);
+// let answer = day24.part1(input);
+// console.log('Part 1', answer);
 
-answer = day24.part2();
+answer = day24.part2(input);
 console.log('Part 2', answer);
