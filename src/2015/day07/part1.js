9410d80daa023ff4cdf877a5b88635b24d9476ec
Tom Wolfe <TOM356@bailliegifford.com>
2018-11-29 14:40:45 +0000

    Day 7.

--- src/2015/day07/part1.js
+++ src/2015/day07/part1.js
@@ -1,28 +1,5 @@
 const string = require('../../util/string');
-
-const funcs = {
-  AND: (l, r) => l & r,
-  OR: (l, r) => l | r,
-  LSHIFT: (l, r) => l << r,
-  RSHIFT: (l, r) => l >>> r
-};
-
-const getValue = (is, w, ws = {}) => {
-  if (!isNaN(w)) { return Number(w); }
-  if (ws[w]) { return ws[w]; }
-
-  const o = is.find(i => i[1] === w);
-
-  if (!isNaN(o[0])) {
-    ws[w] = Number(o[0]);
-  } else {
-    const p = string.words(o[0]);
-    if (p.length === 1) { ws[w] = getValue(is, p[0], ws); }
-    else if (p.length === 2) { ws[w] = ~getValue(is, p[1], ws); }
-    else { ws[w] = funcs[p[1]](getValue(is, p[0], ws), getValue(is, p[2], ws)); }
-  }
-  return ws[w];
-};
+const getValue = require('./get-value');
 
 module.exports = function (input) {
   const i = string.lines(input).map(c => c.split(' -> '));
--- src/2015/day07/part2.js
+++ src/2015/day07/part2.js
@@ -1,3 +1,10 @@
+const string = require('../../util/string');
+const getValue = require('./get-value');
+
 module.exports = function (input) {
- return 0;
+  const i = string.lines(input).map(c => c.split(' -> '));
+  i.forEach(n => {
+    if (n[1] === 'b') {  n[0] = '46065'; }
+  });
+  return getValue(i, 'a');
 }
