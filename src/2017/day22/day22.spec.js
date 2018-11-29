195d72242eee4415b08a34cf04e70ea11f49c695
Tom Wolfe <TOM356@bailliegifford.com>
2018-11-29 14:28:41 +0000

    Get tests working again.

--- src/2017/day22/day22.spec.js
+++ src/2017/day22/day22.spec.js
@@ -3,7 +3,7 @@ const path = require('path');
 const day22 = require('./day22');
 
 describe('day22', () => {
-  
+
   const start = `.........
 .........
 .........
@@ -26,13 +26,11 @@ describe('day22', () => {
   });
 
   describe('part2', () => {
-    it('Produces the correct output 1', () => {
-      it('Produces the correct output 3x3', () => {
-        expect(day22.part2(start, 4, 100)).toBe(26);
-      });
-      it('Produces the correct output 3x3', () => {
-        expect(day22.part2(start, 4, 10000000)).toBe(2511944);
-      });
+    it('Produces the correct output 3x3', () => {
+      expect(day22.part2(start, 4, 100)).toBe(26);
+    });
+    it('Produces the correct output 3x3', () => {
+      expect(day22.part2(start, 4, 10000000)).toBe(2511944);
     });
   });
 });
