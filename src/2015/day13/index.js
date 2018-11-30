669fdc832ad42b42edf87805e2862abdafbfe091
Tom Wolfe <TOM356@bailliegifford.com>
2018-11-30 13:25:43 +0000

    Day 13 boilerplate

--- src/2015/day13/index.js
+++ src/2015/day13/index.js
@@ -2,7 +2,7 @@ const file = require('../../util/file');
 const part1 = require('./part1');
 const part2 = require('./part2');
 
-console.log('  Day 12');
+console.log('  Day 13');
 let input = file.readString(__dirname, 'input.txt');
 
 console.log('    Part 1:', part1(input));
