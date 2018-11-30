4cebd3cb9ed4f356435f33c06ac6a32b891c02b5
Tom Wolfe <TOM356@bailliegifford.com>
2018-11-30 13:23:00 +0000

    Finish day 12 part 2

--- src/2015/day12/day11.spec.js
+++ src/2015/day12/day11.spec.js
@@ -20,4 +20,12 @@ describe('day12', () => {
       expect(part1('[-1,{"a":1}]')).toEqual(0);
     });
   });
+  describe('part2', () => {
+    it('any', () => {
+      expect(part2('[1, 2, 3]')).toEqual(6);
+      expect(part2('[1,{"c":"red","b":2},3]')).toEqual(4);
+      expect(part2('{"d":"red","e":[1,2,3,4],"f":5}')).toEqual(0);
+      expect(part2('[1,"red",5]')).toEqual(6);
+    });
+  });
 });
