fa437139057477f9361e9be6b1f9938638effb3c
Tom Wolfe <trwolfe13@gmail.com>
2018-12-01 11:37:23 +0000

    Day 1

---
+++ src/2018/day01/day01.spec.js
@@ -0,0 +1,11 @@
+const part1 = require('./part1');
+const part2 = require('./part2');
+
+describe('day1', () => {
+  describe('part2', () => {
+    it('', () => {
+      const result = part2('+7\n+7\n-2\n-7\n-4');
+      expect(result).toBe(14);
+    });
+  });
+});
--- src/2018/day01/part2.js
+++ src/2018/day01/part2.js
@@ -1,3 +1,14 @@
+const string = require('../../util/string');
+
 module.exports = function (input) {
-  return undefined;
+  const prev = [0];
+  let acc = 0;
+  const lines = string.lines(input);
+  while(true) {
+    for (let x = 0; x < lines.length; x++) {
+      acc += Number(lines[x]);
+      if (prev.includes(acc)) { return acc; }
+      prev.push(acc);
+    }
+  }
 }
