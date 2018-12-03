c84ac59ab13e40dddf061e6ecaa2ae73ed2516d8
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-03 14:59:18 +0000

    Solve 2015 day 17

--- src/2015/day17/part1.js
+++ src/2015/day17/part1.js
@@ -2,26 +2,28 @@ const _ = require('lodash');
 
 const string = require('../../util/string');
 
-const parse = input => string.lines(input).map(Number);
+const parse = input => string.lines(input).map((n, id) => ({ id, size: Number(n) }));
 
 const permuteSum = (arr, sum, cur = [], all = []) => {
-  arr.forEach((n, i) => {
-    if (n > sum || cur.includes(i)) { return; }
-    const newCur = [...cur, i];
-    if (n === sum) {
-      newCur.sort();
+  arr.forEach(c => {
+    const newCur = [...cur, c.id];
+    if (c.size === sum) {
       if (!all.find(a => _.isEqual(a, newCur))) {
         all.push(newCur);
       }
+      return;
     } else {
-      permuteSum(arr, sum - n, newCur, all);
+      const remaining = arr.filter(n => n.size <= sum && n.id > c.id);
+      if (remaining.length > 0) {
+        permuteSum(remaining, sum - c.size, newCur, all);
+      }
     }
   });
-  return all;//.map(n => n.map(i => arr[i]));
+  return all;
 };
 
 module.exports = function (input) {
-  //const containers = parse(input);
-  const containers = [20, 15, 10, 5, 5];
-  return permuteSum(containers, 25).length;
+  const containers = parse(input);
+  const result = permuteSum(containers, 150);
+  return result.length;
 }
