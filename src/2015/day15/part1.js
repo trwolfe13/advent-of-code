81610c824217a3cd2fd5beb47ee6005df7366e48
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-03 12:30:22 +0000

    Solve day 15 part 2

--- src/2015/day15/part1.js
+++ src/2015/day15/part1.js
@@ -6,5 +6,5 @@ const cookies = require('./cookies');
 module.exports = function (input) {
   const i = cookies.parse(input);
   const perms = number.sumParts(100, i.length);
-  return array.projectReduce(perms, p => cookies.total(i, p).total, (p, c) => c > p).value
+  return array.projectReduce(perms, p => cookies.total(i, p).total, (p, c) => c > p).value;
 }
--- src/2015/day15/part2.js
+++ src/2015/day15/part2.js
@@ -1,3 +1,11 @@
+const array = require('../../util/array');
+const number = require('../../util/number');
+
+const cookies = require('./cookies');
+
 module.exports = function (input) {
-  return undefined;
+  const i = cookies.parse(input);
+  const perms = number.sumParts(100, i.length);
+  const recipes = perms.map(p => cookies.total(i, p)).filter(r => r.calories === 500);
+  return array.projectReduce(recipes, p => p.total, (p, c) => c > p).value;
 }
