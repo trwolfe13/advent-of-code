8394f49e47b2767718461ebb0f6d73bb71c2bb55
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-03 08:06:41 +0000

    Solve day 2 part 12

--- src/2018/day02/part1.js
+++ src/2018/day02/part1.js
@@ -1,11 +1,14 @@
 const string = require('../../util/string');
 
+const hasChars = (input, n) => {
+  return string.chars(input).some(l => input.match(new RegExp(l, 'g')).length === n);
+}
+
 module.exports = function (input) {
   let a = 0, b = 0;
-
   string.lines(input).forEach(c => {
-
+    if (hasChars(c, 2)) { a++; }
+    if (hasChars(c, 3)) { b++; }
   });
-
   return a * b;
 }
