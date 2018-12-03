6b1c7592b04ede765ea33faf32a61b8294c4c7cf
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-03 08:29:18 +0000

    Day 2 part 2

--- src/2018/day02/part2.js
+++ src/2018/day02/part2.js
@@ -1,3 +1,20 @@
+const string = require('../../util/string');
+const _ = require('lodash');
+
+const same = (s1, s2) => {
+  let same = '';
+  for (let x = 0; x < s1.length; x++) {
+    if (s1[x] === s2[x]) { same += s1[x]; }
+  }
+  return same;
+}
+
+const findDiff = (line, list) => list.find(l => same(l, line).length === line.length - 1);
+
 module.exports = function (input) {
-  return undefined;
+  const lines = string.lines(input);
+  for (let x = 0; x < lines.length; x++) {
+    const diff = findDiff(lines[x], lines);
+    if (diff) { return same(diff, lines[x]); }
+  }
 }
