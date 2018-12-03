2839c8b6ec1d7224762b199f625a20169afc3394
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-03 16:23:41 +0000

    Day 18 part 2

--- src/2015/day18/part2.js
+++ src/2015/day18/part2.js
@@ -1,3 +1,19 @@
-module.exports = function (input) {
-  return undefined;
+const lights = require('./lights');
+
+const turnOnCorners = grid => {
+  grid[0][0] = lights.ON;
+  grid[0][99] = lights.ON;
+  grid[99][0] = lights.ON;
+  grid[99][99] = lights.ON;
 }
+
+module.exports = function (input) {
+  let grid = lights.parse(input);
+  turnOnCorners(grid);
+
+  for (let x = 0; x < 100; x++) {
+    grid = lights.step(grid);
+    turnOnCorners(grid);
+  }
+  return lights.count(grid, c => c === lights.ON);
+};
