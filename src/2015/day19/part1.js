b957bf818b9b87aee2bc5944f57e3909d6c0f0cd
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-05 12:55:06 +0000

    Day 19 part 1

--- src/2015/day19/part1.js
+++ src/2015/day19/part1.js
@@ -1,3 +1,30 @@
+
+const _ = require('lodash');
+
+const string = require('../../util/string');
+
+const parse = input => {
+  const result = string.lines(input);
+  return {
+    patterns: result.slice(0, result.length - 2).map(p => p.split(' => ')),
+    molecule: result[result.length - 1]
+  }
+}
+
+const perms = data => {
+  const list = [];
+  data.patterns.forEach(pattern => {
+    const regex = new RegExp(pattern[0], 'g');
+    let match;
+    while (match = regex.exec(data.molecule)) {
+      const newPat = data.molecule.substring(0, match.index) + pattern[1] + data.molecule.substring(match.index + pattern[0].length);
+      if (!list.includes(newPat)) { list.push(newPat); }
+    }
+  });
+  return list;
+}
+
 module.exports = function (input) {
-  return undefined;
+  const data = parse(input)
+  return perms(data).length;
 }
