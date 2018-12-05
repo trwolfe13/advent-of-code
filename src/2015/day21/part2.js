4dfcdbefc838886f29de5c5a01eaa6d386c16c20
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-05 16:55:17 +0000

    Solve day 21.

--- src/2015/day21/part2.js
+++ src/2015/day21/part2.js
@@ -1,3 +1,10 @@
+const _ = require('lodash');
+const rpg = require('./rpg');
+
 module.exports = function (input) {
-  return undefined;
+  const ep = _.orderBy(rpg.allEquipmentPerms(), 'cost', 'desc');
+  for (let x = 0; x < ep.length; x++) {
+    if (!rpg.canWin(ep[x])) { return ep[x].cost; }
+  }
+  return -1;
 }
