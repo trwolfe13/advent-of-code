c3e01a215707204b9363a88a5b06458daf612e1e
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-05 08:12:06 +0000

    Day 5 part 1

--- src/2018/day05/part1.js
+++ src/2018/day05/part1.js
@@ -1,3 +1,7 @@
-module.exports = function (input) {
-  return undefined;
-}
+const string = require('../../util/string');
+
+module.exports = input => string.replaceAll(
+  input,
+  /(Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz|aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ)/g,
+  ''
+).length;
--- src/util/string.js
+++ src/util/string.js
@@ -1,5 +1,13 @@
 module.exports = {
   chars: input => input.split(''),
   words: input => input.split(/\s/g),
-  lines: input => input.match(/[^\r\n]+/g).map(n => n.trim())
+  lines: input => input.match(/[^\r\n]+/g).map(n => n.trim()),
+  replaceAll: (input, search, replace) => {
+    let value = input, last = undefined;
+    do {
+      last = value;
+      value = value.replace(search, replace);
+    } while (value !== last)
+    return value;
+  }
 }
