b7003740e73f9f0391f0a88d8775ae11047cafcf
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-05 15:58:38 +0000

    Day 20 part 1

--- src/2015/day20/part1.js
+++ src/2015/day20/part1.js
@@ -1,3 +1,22 @@
+const housePresents = number => {
+  let presents = 0;
+  for (let x = 1; x <= number; x++) {
+    presents += Number.isInteger(number / x) ? x * 10 : 0;
+  }
+  return presents;
+}
+
+const firstHouse = (target, startAt, skip) => {
+  let x = startAt;
+  while (true) {
+    if (housePresents(x) >= target) { return x; }
+    x += skip;
+  }
+}
+
 module.exports = function (input) {
-  return undefined;
+  const target = Number(input), skip = 200;
+  let x = firstHouse(target, 0, skip);
+  x -= skip;
+  return firstHouse(target, x, 1);
 }
