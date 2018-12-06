e5792b11fb46fe6bd9b9b4977fa494516964495b
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-06 17:01:31 +0000

    Start day 23

--- src/2015/day23/part1.js
+++ src/2015/day23/part1.js
@@ -1,3 +1,13 @@
+const parse = input => string.lines(input).map(l => ({ instruction: l.substring(0, 3), params: l.substring(4).split(', ') }));
+
+
+const instructions = {
+  hlf: s => s.r /= 2,
+  tpl: s => s.r *= 3
+  inc: 
+}
+
+
 module.exports = function (input) {
   return undefined;
 }
