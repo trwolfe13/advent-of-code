7473237b466476720d317c21b39ccda13043d5d1
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-06 16:46:59 +0000

    Solve day 22 part 1

--- src/2015/day22/part1.js
+++ src/2015/day22/part1.js
@@ -4,8 +4,13 @@ const rpg = require('./rpg');
 module.exports = function (input) {
   const player = { hp: 50, armor: 0, mana: 500 };
   const boss = { hp: 51, damage: 9 };
-  const perms = rpg.allSpellPerms(player, boss, player.mana, 3);
-  const results = perms.map(rpg.result).filter(r => r.win);
-  
+
+  // const player = { hp: 10, armor: 0, mana: 250 };
+  // const boss = { hp: 14, damage: 8 };
+
+  const perms = rpg.allSpellPerms(player, boss, 10);
+  // console.log(perms.map(p => p.map(s => s.name)));
+  const results = perms.map(p => rpg.result(p, player, boss)).filter(r => r.win);
+
   return _.min(results.map(r => r.cost));
 }
--- src/2015/day22/rpg.js
+++ src/2015/day22/rpg.js
@@ -5,52 +5,101 @@ const spells = [
   { name: 'Drain', duration: 1, cost: 73, cast: (a, d) => { d.hp -= 2; a.hp += 2; }, each: (a, d) => { }, end: (a, d) => { } },
   { name: 'Shield', duration: 6, cost: 113, cast: (a, d) => a.armor += 7, each: (a, d) => { }, end: (a, d) => a.armor -= 7 },
   { name: 'Poison', duration: 6, cost: 173, cast: (a, d) => { }, each: (a, d) => d.hp -= 3, end: (a, d) => { } },
-  { name: 'Recharge', duration: 5, cost: 229, cast: (a, d) => { }, each: (a, d) => d.mana += 101, end: (a, d) => { } }
+  { name: 'Recharge', duration: 5, cost: 229, cast: (a, d) => { }, each: (a, d) => a.mana += 101, end: (a, d) => { } }
 ];
 
-const spellEffect = spell => ({
-  name: spell.name,
-  timer: spell.duration,
-  each: spell.each,
-  end: spell.end
-});
-
-const allSpellPerms = (player, boss, mana, max, current = [], effects = [], perms = []) => {
-  if (current.length === max) {
-    perms.push([...current]); current.splice(0, current.length);
-    return perms;
-  }
-
+processEffects = (effects, player, boss) => {
   effects.forEach(e => {
     e.each(player, boss);
     e.timer--;
   });
-  effects = effects.filter(e => e.timer > 0);
+  for (let x = effects.length - 1; x >= 0; x--) {
+    if (effects[x].timer <= 0) {
+      effects[x].end(player, boss);
+      effects.splice(x, 1);
+    }
+  }
+}
 
-  const available = availableSpells(spells, mana, effects);
+const castSpell = (player, spell, boss) => {
+  spell.cast(player, boss);
+  player.mana -= spell.cost;
+  return {
+    name: spell.name,
+    timer: spell.duration,
+    each: spell.each,
+    end: spell.end
+  };
+}
+
+const allSpellPerms = (player, boss, max, current = [], effects = [], perms = []) => {
+  if (current.length === max) {
+    perms.push(current);
+    return perms;
+  }
+  processEffects(effects, player, boss);
+  processEffects(effects, player, boss);
+  const available = availableSpells(spells, player, effects);
+
+  if (available.length === 0) {
+    perms.push(current);
+    return;
+  }
 
   available.forEach(spell => {
-    spell.cast(player, boss);
+    const tPlayer = { ...player }, tBoss = { ...boss };
+    const effect = castSpell(tPlayer, spell, tBoss);
     const spellList = [...current, spell];
-    const newEffects = _.cloneDeep([...effects, spellEffect(spell)]);
-    allSpellPerms(player, boss, mana - spell.cost, max, spellList, newEffects, perms);
+    const newEffects = _.cloneDeep([...effects, effect]);
+    allSpellPerms(tPlayer, tBoss, max, spellList, newEffects, perms);
   });
   return perms;
 }
 
-const availableSpells = (spells, mana, effects) => {
+const availableSpells = (spells, player, effects) => {
   const eNames = effects.map(e => e.name);
-  return spells.filter(s => s.cost <= mana && !eNames.includes(s.name))
+  return spells.filter(s => s.cost <= player.mana && !eNames.includes(s.name))
 };
 
-result = spells => {
+result = (spells, playerModel, bossModel) => {
   // TODO: Implement this method.
-  const player = { hp: 50, armor: 0, mana: 500 };
-  const boss = { hp: 51, damage: 9 };
-  return { win: true, cost: _.sum(spells.map(s => s.cost)) };
+  const effects = [];
+  const cast = [];
+  const player = { ...playerModel };
+  const boss = { ...bossModel };
+
+  let turn = 0;
+  for (turn = 0; turn < spells.length; turn++) {
+    // Effects happen. 
+    processEffects(effects, player, boss);
+
+    // Player's turn.
+    const spell = spells[turn];
+    effects.push(castSpell(player, spell, boss));
+    cast.push(spell);
+
+    if (boss.hp <= 0) { break; }
+
+    // Effects happen. 
+    processEffects(effects, player, boss);
+    if (boss.hp <= 0) { break; }
+
+    // Boss's turn.
+    player.hp -= Math.max(1, boss.damage - player.armor);
+
+    if (player.hp <= 0) { break; }
+  }
+
+  const win = boss.hp <= 0 && player.hp > 0;
+  const cost = _.sum(cast.map(s => s.cost));
+
+  // console.log('Win:', win, 'Cast:', cast.map(c => c.name), 'Cost: ', cost);
+
+  return { win, cost };
 }
 
 module.exports = {
+  spells,
   allSpellPerms,
   result
 };
