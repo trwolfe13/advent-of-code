3b0dcf750969ff83ffd0d5677a5e75cdddaaf52c
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-07 16:24:57 +0000

    Day 2

--- src/2016/day02/part2.js
+++ src/2016/day02/part2.js
@@ -1,3 +1,26 @@
+const string = require('../../util/string');
+
 module.exports = function (input) {
-  return undefined;
+  const keypad = [
+    [' ', ' ', '1', ' ', ' '],
+    [' ', '2', '3', '4', ' '],
+    ['5', '6', '7', '8', '9'],
+    [' ', 'A', 'B', 'C', ' '],
+    [' ', ' ', 'D', ' ', ' '],
+  ];
+  const pos = [2, 1];
+  let code = '';
+
+  string.lines(input).forEach(l => {
+    string.chars(l).forEach(c => {
+      switch (c) {
+        case 'U': pos[0] = Math.max(Math.abs(pos[1] - 2), pos[0] - 1); break;
+        case 'D': pos[0] = Math.min(4 - Math.abs(pos[1] - 2), pos[0] + 1); break;
+        case 'L': pos[1] = Math.max(Math.abs(pos[0] - 2), pos[1] - 1); break;
+        case 'R': pos[1] = Math.min(4 - Math.abs(pos[0] - 2), pos[1] + 1); break;
+      }
+    });
+    code += keypad[pos[0]][pos[1]];
+  });
+  return code;
 }
