5979a87ded95d8db0ea36564f869cdf0f271ba25
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-07 12:33:45 +0000

    Day 25 part 1

--- src/2015/day25/part1.js
+++ src/2015/day25/part1.js
@@ -1,3 +1,12 @@
+const index = (row, column) => 0.5 * Math.pow(column, 2) + (row - 0.5) * column + 0.5 * Math.pow(row, 2) - 1.5 * row + 1;
+const code = index => {
+  let i = 1, c = 20151125;
+  while (i < index) { c *= 252533; c %= 33554393; i++; }
+  return c;
+}
+
 module.exports = function (input) {
-  return undefined;
+  const row = 2978, column = 3083;
+  const idx = index(row, column);
+  return code(idx);
 }
