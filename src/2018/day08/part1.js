d72dfee6bed73235f3249006ebf1caa683205a46
Tom Wolfe <trwolfe13@gmail.com>
2018-12-08 14:33:37 +0000

    Day 8 part 1

--- src/2018/day08/part1.js
+++ src/2018/day08/part1.js
@@ -1,3 +1,12 @@
+countNode = (stream, state = { cursor: 0, total: 0 }) => {
+  const children = stream[state.cursor++];
+  const metadata = stream[state.cursor++];
+  for (let x = 0; x < children; x++) { countNode(stream, state); }
+  for (let x = 0; x < metadata; x++) { state.total += stream[state.cursor++]; }
+  return state;
+}
+
 module.exports = function (input) {
-  return undefined;
+  const stream = input.split(' ').map(Number);
+  return countNode(stream).total;
 }
