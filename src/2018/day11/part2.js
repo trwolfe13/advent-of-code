48ab8c55787f5eec6d2aa8ee0ddc34be2d3a1953
Tom Wolfe <trwolfe13@gmail.com>
2018-12-11 05:55:25 +0000

    Day 11

--- src/2018/day11/part2.js
+++ src/2018/day11/part2.js
@@ -1,3 +1,16 @@
+const array = require('../../util/array');
+const power = require('./power');
+
 module.exports = function (input) {
-  return undefined;
+  const grid = array.grid(300, 300);
+  const serial = Number(input);
+  const p = power.powerMap(grid, serial);
+  let lSquare;
+  for (let size = 1; size <= p.length; size++) {
+    const n = power.largestSquare(p, size);
+    if (!lSquare || n.total > lSquare.total) {
+      lSquare = n;
+    }
+  }
+  return `${lSquare.coords[0]},${lSquare.coords[1]},${lSquare.size}}`;
 }
