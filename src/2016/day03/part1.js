fb76d7f2b72d1191983caadce43c1a0bcc14a760
Tom Wolfe <TOM356@bailliegifford.com>
2018-12-11 12:21:53 +0000

    Day 3

--- src/2016/day03/part1.js
+++ src/2016/day03/part1.js
@@ -1,3 +1,11 @@
+const string = require('../../util/string')
+const _ = require('lodash');
+
+const parse = input => string.lines(input).map(line => line.split(' ').map(Number));
+
+const isPossible = triangle => { const max = _.max(triangle); return max < _.sum(triangle) - max; }
+
 module.exports = function (input) {
-  return undefined;
+  const triangles = parse(input);
+  return triangles.filter(isPossible).length;
 }
