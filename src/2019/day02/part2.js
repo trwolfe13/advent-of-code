6b65410d19214c1985aa2228c3580c3818898950
Tom Wolfe <twolfe@scottlogic.com>
2019-12-02 11:33:46 +0000

    Complete day 2

--- src/2019/day02/part2.js
+++ src/2019/day02/part2.js
@@ -1,3 +1,20 @@
+const intcode = require('./intcode');
+
 module.exports = function (input) {
+  const program = intcode.parse(input);
+
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const memory = [...program];
+      memory[1] = noun;
+      memory[2] = verb;
+      intcode.execute(memory);
+
+      if (memory[0] === 19690720) {
+        return 100 * noun + verb;
+      }
+    }
+  }
+
   return undefined;
 }
